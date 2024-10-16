514dc77921ec7e63a55b372a36f7b1fcd1fa43f2
pa-nov <novpavel06@gmail.com>
2024-10-17 01:11:04 +0300

    Created: volume script

---
+++ config/hypr/scripts/volume.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pactl subscribe | grep --line-buffered "Event 'change' on sink" | while read -r event; do
+    volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+    muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+    value=$(echo "$volume 100" | awk '{print $1*$2}')
+    if [ $muted == "yes" ]; then
+        notify-send -u low -t 1000 -h int:value:$value -r 1000 "Volume: Muted"
+    else
+        notify-send -u low -t 1000 -h int:value:$value -r 1000 "Volume: $value%"
+    fi
+done
