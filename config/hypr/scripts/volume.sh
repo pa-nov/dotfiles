aa784665179cc26394f2c9fbff7643c16c42e39f
pa-nov <novpavel06@gmail.com>
2024-10-17 22:22:15 +0300

    Updated: volume script

--- config/hypr/scripts/volume.sh
+++ config/hypr/scripts/volume.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-old=""
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+previous="$volume $muted"
 
 pactl subscribe | grep --line-buffered "Event 'change' on sink" | while read -r event; do
     volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
     muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
-    value=$(echo "$volume 100" | awk '{print $1*$2}')
-    if [ "$old" != "$muted $value" ]; then
-        old="$muted $value"
+    if [ "$previous" != "$volume $muted" ]; then
+        previous="$volume $muted"
+        value=$(echo "$volume 100" | awk '{print $1*$2}')
         if [ "$muted" == "yes" ]; then
             notify-send -u low -t 1000 -h int:value:$value -r 1000 "Volume: Muted"
         else
