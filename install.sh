0048fbb6166c50ffd8f671670f16bdc5ed50809b
pa-nov <novpavel06@gmail.com>
2024-12-04 20:18:45 +0300

    Updated: install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S noto-fonts-emoji otf-font-awesome ttf-roboto ttf-jetbrains-mono --noconfirm --needed
 sudo pacman -S polkit-kde-agent dunst grim slurp wofi --noconfirm --needed
-sudo pacman -S gvfs cliphist blueman qt5ct qt6ct nwg-look --noconfirm --needed
+sudo pacman -S gvfs gnome-keyring cliphist blueman qt5ct qt6ct nwg-look --noconfirm --needed
 
 sudo pacman -S hyprpaper hyprpicker hyprlock waybar --noconfirm --needed
 sudo pacman -S zip unzip xarchiver --noconfirm --needed
