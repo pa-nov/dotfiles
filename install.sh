f95b5854c4af8ad7699bffe784a398d138a7db02
pa-nov <novpavel06@gmail.com>
2025-08-07 03:41:07 +0300

    Updated: install script

--- install.sh
+++ install.sh
@@ -7,15 +7,19 @@ install_packages() {
     sudo pacman -D --noconfirm --quiet --asexplicit $@
 }
 
-echo "[$title] Started: Installation"
+echo "[${title}] Started: Installation"
 
 sudo cp ./etc/pacman.conf /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
-install_packages openssh curl wget socat nano htop git
-install_packages bash-completion brightnessctl zip unzip
+install_packages openssh curl wget socat man-db nano htop dmidecode git
+install_packages bash-completion man-pages brightnessctl zip unzip
+install_packages libcanberra imagemagick ffmpeg gst-libav gst-plugins-base
+install_packages gst-plugins-ugly gst-plugins-bad gst-plugins-good
+
 install_packages orchis-theme tela-circle-icon-theme-standard
-install_packages noto-fonts-emoji otf-font-awesome ttf-roboto ttf-jetbrains-mono
+install_packages noto-fonts-emoji otf-font-awesome
+install_packages ttf-roboto ttf-jetbrains-mono
 
 install_packages tk gtk3 gtk4 qt5-wayland qt6-wayland hyprland
 install_packages xdg-utils xdg-desktop-portal-hyprland grim slurp
@@ -23,23 +27,19 @@ install_packages hyprlock hyprpaper hyprpicker hyprpolkitagent
 install_packages waybar wofi wtype dunst gvfs gnome-keyring cliphist
 install_packages networkmanager network-manager-applet blueman
 
-install_packages kitty libcanberra
-install_packages thunar thunar-archive-plugin xarchiver
-install_packages mousepad ristretto vlc gparted
+install_packages kitty thunar thunar-archive-plugin xarchiver
+install_packages mousepad ristretto parole gparted
+install_packages qemu-desktop gimp steam
 
-# sudo pacman -S code
-# sudo pacman -S qemu-desktop
-# sudo pacman -S steam gamescope
-
-echo "[$title] Finished: Installation"
+echo "[${title}] Finished: Installation"
 
 while true; do
-    read -r -p "[$title] Run configuration script now? [y/n] " result
+    read -r -p "[${title}] Run configuration script now? [y/n] " result
     if [[ "$result" == [Yy]* ]]; then
         ./setup.sh
         break
     elif [[ "$result" == [Nn]* ]]; then
-        read -r -s -n 1 -p "[$title] Press any key to continue..." && echo
+        read -r -s -n 1 -p "[${title}] Press any key to continue..." && echo
         break
     fi
 done
