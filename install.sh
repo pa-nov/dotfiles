4a0fd87a54cfc1fa68a9ad2cf60d177b482674b1
pa-nov <novpavel06@gmail.com>
2026-01-23 09:43:35 +0300

    Updated: install.sh

--- install.sh
+++ install.sh
@@ -10,26 +10,27 @@ install_packages() {
 echo "[${title}] Started: Installation"
 
 sudo cp ./etc/pacman.conf /etc/pacman.conf
-sudo pacman -Syu --noconfirm
+sudo pacman -Syyu --noconfirm
 
 install_packages openssh curl wget socat man-db nano htop dmidecode git
-install_packages bash-completion man-pages brightnessctl zip unzip
+install_packages bash-completion man-pages inxi brightnessctl zip unzip
 install_packages libcanberra imagemagick ffmpeg gst-libav gst-plugins-base
 install_packages gst-plugins-ugly gst-plugins-bad gst-plugins-good
+install_packages dosfstools exfatprogs e2fsprogs
 
 install_packages orchis-theme tela-circle-icon-theme-standard
 install_packages noto-fonts-emoji otf-font-awesome
-install_packages ttf-roboto ttf-jetbrains-mono
+install_packages ttf-roboto ttf-liberation ttf-jetbrains-mono
 
 install_packages tk gtk3 gtk4 qt5-wayland qt6-wayland hyprland
-install_packages xdg-utils xdg-desktop-portal-hyprland grim slurp
-install_packages hyprlock hyprpaper hyprpicker hyprpolkitagent
+install_packages flatpak xdg-utils xdg-desktop-portal-hyprland grim slurp
+install_packages hypridle hyprlock hyprpaper hyprpicker hyprpolkitagent
 install_packages waybar wofi wtype dunst gvfs gnome-keyring cliphist
 install_packages networkmanager network-manager-applet blueman
 
 install_packages kitty thunar thunar-archive-plugin xarchiver
-install_packages mousepad ristretto parole gparted
-install_packages qemu-desktop gimp steam
+install_packages mousepad ristretto parole gparted fwupd
+install_packages galculator gcolor3 gimp mission-center qemu-desktop
 
 echo "[${title}] Finished: Installation"
 
